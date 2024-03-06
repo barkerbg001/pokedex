4045d6995ba6b864fcf72f3c6d12556fae6bee4c
Bradley <barkerbg001@gmail.com>
2024-03-06 22:05:23 +0200

    getting images now from the api and displaying it in a tile format

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,25 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './Pokedex.css';
 
 function Pokedex() {
     const [pokemons, setPokemons] = useState([]);
 
     useEffect(() => {
         axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-            .then(response => {
-                setPokemons(response.data.results);
+          .then(response => {
+            const promises = response.data.results.map(pokemon => axios.get(pokemon.url));
+            Promise.all(promises).then(results => {
+              const pokemonData = results.map(result => result.data);
+              setPokemons(pokemonData);
             });
-    }, []);
+          });
+      }, []);
+      
 
-    return (
+      return (
         <div className="pokedex">
-            <ul>
-                {pokemons.map(pokemon => (
-                    <li key={pokemon.name}>{pokemon.name}</li>
-                ))}
-            </ul>
+          <div className="pokemon-grid">
+            {pokemons.map(pokemon => (
+              <div key={pokemon.id} className="pokemon-card">
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                <p>{pokemon.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
-    );
+      );
 }
 
 export default Pokedex;
