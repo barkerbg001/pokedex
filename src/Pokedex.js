5cd592a8a25ed0b15c04670e98265167f3f41a22
Bradley <barkerbg001@gmail.com>
2024-03-07 20:53:21 +0200

    Added Search Bar

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,32 +3,44 @@ import axios from 'axios';
 import './Pokedex.css';
 
 function Pokedex() {
-    const [pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
-    useEffect(() => {
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-          .then(response => {
-            const promises = response.data.results.map(pokemon => axios.get(pokemon.url));
-            Promise.all(promises).then(results => {
-              const pokemonData = results.map(result => result.data);
-              setPokemons(pokemonData);
-            });
-          });
-      }, []);
-      
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => {
+        const promises = response.data.results.map(pokemon => axios.get(pokemon.url));
+        Promise.all(promises).then(results => {
+          const pokemonData = results.map(result => result.data);
+          setPokemons(pokemonData);
+        });
+      });
+  }, []);
 
-      return (
-        <div className="pokedex">
-          <div className="pokemon-grid">
-            {pokemons.map(pokemon => (
-              <div key={pokemon.id} className="pokemon-card">
-                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                <p>{pokemon.name}</p>
-              </div>
-            ))}
+  function handleSearchChange(event) {
+    setSearchQuery(event.target.value.toLowerCase());
+  }
+
+
+
+  return (
+    <div className="pokedex">
+      <input
+        type="text"
+        placeholder="Search Pokémon"
+        className="search-input"
+        onChange={handleSearchChange} // We will define this function next
+      />
+      <div className="pokemon-grid">
+        {pokemons.filter(pokemon => pokemon.name.includes(searchQuery)).map(pokemon => (
+          <div key={pokemon.id} className="pokemon-card">
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <p>{pokemon.name}</p>
           </div>
-        </div>
-      );
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Pokedex;
